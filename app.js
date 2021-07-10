ae99918c9b2050fbf176e3b111489a28c704a62a
shadrach247 <soshadrach@gmail.com>
2021-07-10 07:39:53 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, Shadrach Ozavehe Onimisi!'));
+app.get('/', (req, res) => res.send('Hello, Shadrach Ozavehe Onimisi; Congratulations, on your first DevOps deployment on AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
